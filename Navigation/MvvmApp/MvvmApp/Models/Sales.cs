5a42447ba1171391cd986c30c216ebb6fc36b0c6
simond <simondi@consumerprotectionbc.ca>
2017-09-01 16:42:02 -0700

    Adding sales

---
+++ Navigation/MvvmApp/MvvmApp/Models/Sales.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//https://www.youtube.com/watch?v=USn6hgk-tLU
+namespace MvvmApp.Models
+{
+    class Sales
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+
+    }
+}
