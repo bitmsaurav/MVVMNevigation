1685e6e449018cf13ca6b0366c0f9766bf0a707a
simond <simondi@consumerprotectionbc.ca>
2017-09-01 16:22:22 -0700

    Inital Commit

---
+++ Navigation/PrismApp/PrismApp/ViewModels/FirstPageViewModel.cs
@@ -0,0 +1,34 @@
+﻿
+using Prism.Commands;
+using Prism.Mvvm;
+using Prism.Navigation;
+
+namespace PrismApp.ViewModels
+{
+    public class FirstPageViewModel : BindableBase, INavigationAware
+    {
+        private readonly INavigationService _navigationService;
+
+        public DelegateCommand NavigateBackCommand { get; private set; }
+
+        public FirstPageViewModel(INavigationService navigationService)
+        {
+            _navigationService = navigationService;
+
+            NavigateBackCommand = new DelegateCommand(async() => 
+                await  _navigationService.GoBackAsync());
+        }
+
+        public void OnNavigatedFrom(NavigationParameters parameters)
+        {
+        }
+
+        public void OnNavigatedTo(NavigationParameters parameters)
+        {
+            if (parameters.ContainsKey("name"))
+            {
+                var name = (string)parameters["name"];
+            }
+        }
+    }
+}
